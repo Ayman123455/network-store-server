cf5bb247d9ca8ee93fa76e88818ee33b44d13596
Ayman123455 <aymenadel1234@gmail.com>
2026-09-12 00:41:35 +0300

    ت dd

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ initializeApp({
 
 const db = getFirestore();
 const app = express();
-//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -138,7 +138,7 @@ app.post('/api/sms-webhook', async (req, res) => {
     return res.status(500).json({ error: 'حدث خطأ داخلي' });
   }
 });
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 السيرفر يعمل بنجاح على: http://localhost:${PORT}`);
   console.log(`🖥️ لوحة التحكم متاحة على: http://localhost:${PORT}/admin`);
